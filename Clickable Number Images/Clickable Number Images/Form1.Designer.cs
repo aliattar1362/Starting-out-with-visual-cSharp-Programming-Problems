f99e25cf33e4c28a312604cda15253645e04b32a
aliattar1362 <123591301+aliattar1362@users.noreply.github.com>
2023-03-29 15:22:09 +0300

    Add files via upload

---
+++ Clickable Number Images/Clickable Number Images/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Clickable_Number_Images
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fivePictureBox = new System.Windows.Forms.PictureBox();
+            this.fourPictureBox = new System.Windows.Forms.PictureBox();
+            this.threePictureBox = new System.Windows.Forms.PictureBox();
+            this.twoPictureBox = new System.Windows.Forms.PictureBox();
+            this.onePictureBox = new System.Windows.Forms.PictureBox();
+            this.oneLabel = new System.Windows.Forms.Label();
+            this.twoLabel = new System.Windows.Forms.Label();
+            this.threeLabel = new System.Windows.Forms.Label();
+            this.fourLabel = new System.Windows.Forms.Label();
+            this.fiveLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fivePictureBox
+            // 
+            this.fivePictureBox.Image = global::Clickable_Number_Images.Properties.Resources._5;
+            this.fivePictureBox.Location = new System.Drawing.Point(554, 116);
+            this.fivePictureBox.Name = "fivePictureBox";
+            this.fivePictureBox.Size = new System.Drawing.Size(100, 50);
+            this.fivePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fivePictureBox.TabIndex = 4;
+            this.fivePictureBox.TabStop = false;
+            this.fivePictureBox.Click += new System.EventHandler(this.fivePictureBox_Click);
+            // 
+            // fourPictureBox
+            // 
+            this.fourPictureBox.Image = global::Clickable_Number_Images.Properties.Resources._4;
+            this.fourPictureBox.Location = new System.Drawing.Point(418, 116);
+            this.fourPictureBox.Name = "fourPictureBox";
+            this.fourPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.fourPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fourPictureBox.TabIndex = 3;
+            this.fourPictureBox.TabStop = false;
+            this.fourPictureBox.Click += new System.EventHandler(this.fourPictureBox_Click);
+            // 
+            // threePictureBox
+            // 
+            this.threePictureBox.Image = global::Clickable_Number_Images.Properties.Resources._3;
+            this.threePictureBox.Location = new System.Drawing.Point(286, 116);
+            this.threePictureBox.Name = "threePictureBox";
+            this.threePictureBox.Size = new System.Drawing.Size(100, 50);
+            this.threePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.threePictureBox.TabIndex = 2;
+            this.threePictureBox.TabStop = false;
+            this.threePictureBox.Click += new System.EventHandler(this.threePictureBox_Click);
+            // 
+            // twoPictureBox
+            // 
+            this.twoPictureBox.Image = global::Clickable_Number_Images.Properties.Resources._2;
+            this.twoPictureBox.Location = new System.Drawing.Point(152, 116);
+            this.twoPictureBox.Name = "twoPictureBox";
+            this.twoPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.twoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.twoPictureBox.TabIndex = 1;
+            this.twoPictureBox.TabStop = false;
+            this.twoPictureBox.Click += new System.EventHandler(this.twoPictureBox_Click);
+            // 
+            // onePictureBox
+            // 
+            this.onePictureBox.Image = global::Clickable_Number_Images.Properties.Resources._1;
+            this.onePictureBox.Location = new System.Drawing.Point(12, 115);
+            this.onePictureBox.Name = "onePictureBox";
+            this.onePictureBox.Size = new System.Drawing.Size(100, 50);
+            this.onePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.onePictureBox.TabIndex = 0;
+            this.onePictureBox.TabStop = false;
+            this.onePictureBox.Click += new System.EventHandler(this.onePictureBox_Click);
+            // 
+            // oneLabel
+            // 
+            this.oneLabel.AutoSize = true;
+            this.oneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oneLabel.Location = new System.Drawing.Point(43, 229);
+            this.oneLabel.Name = "oneLabel";
+            this.oneLabel.Size = new System.Drawing.Size(35, 16);
+            this.oneLabel.TabIndex = 5;
+            this.oneLabel.Text = "One";
+            this.oneLabel.Visible = false;
+            // 
+            // twoLabel
+            // 
+            this.twoLabel.AutoSize = true;
+            this.twoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.twoLabel.Location = new System.Drawing.Point(189, 229);
+            this.twoLabel.Name = "twoLabel";
+            this.twoLabel.Size = new System.Drawing.Size(36, 16);
+            this.twoLabel.TabIndex = 6;
+            this.twoLabel.Text = "Two";
+            this.twoLabel.Visible = false;
+            // 
+            // threeLabel
+            // 
+            this.threeLabel.AutoSize = true;
+            this.threeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.threeLabel.Location = new System.Drawing.Point(319, 229);
+            this.threeLabel.Name = "threeLabel";
+            this.threeLabel.Size = new System.Drawing.Size(48, 16);
+            this.threeLabel.TabIndex = 7;
+            this.threeLabel.Text = "Three";
+            this.threeLabel.Visible = false;
+            // 
+            // fourLabel
+            // 
+            this.fourLabel.AutoSize = true;
+            this.fourLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fourLabel.Location = new System.Drawing.Point(450, 231);
+            this.fourLabel.Name = "fourLabel";
+            this.fourLabel.Size = new System.Drawing.Size(38, 16);
+            this.fourLabel.TabIndex = 8;
+            this.fourLabel.Text = "Four";
+            this.fourLabel.Visible = false;
+            // 
+            // fiveLabel
+            // 
+            this.fiveLabel.AutoSize = true;
+            this.fiveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fiveLabel.Location = new System.Drawing.Point(589, 231);
+            this.fiveLabel.Name = "fiveLabel";
+            this.fiveLabel.Size = new System.Drawing.Size(37, 16);
+            this.fiveLabel.TabIndex = 9;
+            this.fiveLabel.Text = "Five";
+            this.fiveLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fiveLabel);
+            this.Controls.Add(this.fourLabel);
+            this.Controls.Add(this.threeLabel);
+            this.Controls.Add(this.twoLabel);
+            this.Controls.Add(this.oneLabel);
+            this.Controls.Add(this.fivePictureBox);
+            this.Controls.Add(this.fourPictureBox);
+            this.Controls.Add(this.threePictureBox);
+            this.Controls.Add(this.twoPictureBox);
+            this.Controls.Add(this.onePictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox onePictureBox;
+        private System.Windows.Forms.PictureBox twoPictureBox;
+        private System.Windows.Forms.PictureBox threePictureBox;
+        private System.Windows.Forms.PictureBox fourPictureBox;
+        private System.Windows.Forms.PictureBox fivePictureBox;
+        private System.Windows.Forms.Label oneLabel;
+        private System.Windows.Forms.Label twoLabel;
+        private System.Windows.Forms.Label threeLabel;
+        private System.Windows.Forms.Label fourLabel;
+        private System.Windows.Forms.Label fiveLabel;
+    }
+}
+
---
+++ Clickable Number Images/Clickable Number Images/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clickable_Number_Images
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void onePictureBox_Click(object sender, EventArgs e)
+        {
+            oneLabel.Visible = true; // Show "One" in label one.
+            twoLabel.Visible = false; // Hide "Two" in label two.
+            threeLabel.Visible = false; // Hide "Three" in label three.
+            fourLabel.Visible = false; // Hide "Four" in label four.
+            fiveLabel.Visible = false; // Hide "Five" in label five.
+        }
+
+        private void twoPictureBox_Click(object sender, EventArgs e)
+        {
+            oneLabel.Visible = false; // Hide "One" in label one.
+            twoLabel.Visible = true; // Show "Two" in label two.
+            threeLabel.Visible = false; // Hide "Three" in label three.
+            fourLabel.Visible = false; // Hide "Four" in label four.
+            fiveLabel.Visible = false; // Hide "Five" in label five.
+        }
+
+        private void threePictureBox_Click(object sender, EventArgs e)
+        {
+            oneLabel.Visible = false; // Hide "One" in label one.
+            twoLabel.Visible = false; // Hide "Two" in label two.
+            threeLabel.Visible = true; // Show "Three" in label three.
+            fourLabel.Visible = false; // Hide "Four" in label four.
+            fiveLabel.Visible = false; // Hide "Five" in label five.
+        }
+
+        private void fourPictureBox_Click(object sender, EventArgs e)
+        {
+            oneLabel.Visible = false; // Hide "One" in label one.
+            twoLabel.Visible = false; // Hide "Two" in label two.
+            threeLabel.Visible = false; // Hide "Three" in label three.
+            fourLabel.Visible = true; // Show "Four" in label four.
+            fiveLabel.Visible = false; // Hide "Five" in label five.
+        }
+
+        private void fivePictureBox_Click(object sender, EventArgs e)
+        {
+            oneLabel.Visible = false; // Hide "One" in label one.
+            twoLabel.Visible = false; // Hide "Two" in label two.
+            threeLabel.Visible = false; // Hide "Three" in label three.
+            fourLabel.Visible = false; // Hide "Four" in label four.
+            fiveLabel.Visible = true; // Show "Five" in label five.
+        }
+    }
+}
